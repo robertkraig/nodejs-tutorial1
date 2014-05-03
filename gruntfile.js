2475812e44f434524e55f8b00838022338805da0
Robert Kraig <robert.kraig@5thkind.com>
2014-05-03 13:10:59 -0700

    more updates

--- gruntfile.js
+++ gruntfile.js
@@ -16,7 +16,8 @@ module.exports = function(grunt)
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-less');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	grunt.registerTask('default',['less']);
+	grunt.registerTask('default',['less','watch']);
 
 };
